b5b107edcc2154519e898f2a157645c71a4bf8d7
ValdianoRocha <dev.valdiano@gmail.com>
2025-04-08 22:57:25 -0300

    ler tarefas 100%

--- functions.js
+++ functions.js
@@ -1,9 +1,8 @@
-
 import fs from "fs/promises"
 const caminho = './tarefas.json'
 
 
-async function lerConverterTarefa() {
+export async function lerConverterTarefa() {
     try {
         const dados = await fs.readFile(caminho, "utf-8")  // ler dados 
         return JSON.parse(dados) //converter em object
@@ -13,5 +12,19 @@ async function lerConverterTarefa() {
     }
 } // ler o arquivi e converte em object
 
-console.log(await lerConverterTarefa());
+async function escreverTarefa(dados) {
+    try { //serve para tratar erro
+        dados = JSON.stringify(dados, null, 2);
+        await fs.writeFile(caminho, dados, 'utf-8')
+        console.log("Arquivo atualizado com sucesso!")
+    } catch (erro) {
+        console.error("Erro ao escrever no arquivo: ", erro);
+    }
+} //reescreve o  arquivo e organiza 
+
+
+
+
+
+// console.log(await lerConverterTarefa());
 
