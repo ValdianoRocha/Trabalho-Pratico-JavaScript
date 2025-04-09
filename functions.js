4b3f42c75cf0a1e2c9a14949455b0bcf8d8a2399
ValdianoRocha <dev.valdiano@gmail.com>
2025-04-08 23:26:21 -0300

    criação da função adicionar-tarefa

--- functions.js
+++ functions.js
@@ -1,5 +1,7 @@
 import fs from "fs/promises"
 const caminho = './tarefas.json'
+import PromptSync from "prompt-sync"
+const prompt = PromptSync()
 
 
 export async function lerConverterTarefa() {
@@ -12,7 +14,7 @@ export async function lerConverterTarefa() {
     }
 } // ler o arquivi e converte em object
 
-async function escreverTarefa(dados) {
+export async function escreverTarefa(dados) {
     try { //serve para tratar erro
         dados = JSON.stringify(dados, null, 2);
         await fs.writeFile(caminho, dados, 'utf-8')
@@ -22,8 +24,22 @@ async function escreverTarefa(dados) {
     }
 } //reescreve o  arquivo e organiza 
 
+export async function adicionarNovaTarefa() {
+    // let dadosInformados = await adicionar()
+    const tarefa = await lerConverterTarefa()
+    const ultimoId = tarefa.length > 0 ? tarefa[tarefa.length - 1].id + 1 : 1; //pegar o ultimo id 
+    // let id = object['animais'].length+1
+    let novaTarefa = {
+        "id": ultimoId,
+        "titulo": prompt("nome: "),
+        "descricao": prompt("tipo: "),
+        "concluida": false
+    }
 
+    tarefa.push(novaTarefa)
+    await escreverTarefa(tarefa)
 
+} //adicionar um novo arquivo no final da lista de objeto usando a função escreverArquivo
 
 
 // console.log(await lerConverterTarefa());
