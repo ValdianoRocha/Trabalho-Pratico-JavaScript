55ae8ea1f10026fe67e2097df8833644febbe473
ValdianoRocha <dev.valdiano@gmail.com>
2025-04-09 00:00:50 -0300

    criação dos import no myProject

---
+++ myProject.js
@@ -0,0 +1,4 @@
+import { lerConverterTarefa } from "./functions.js";
+import { adicionarNovaTarefa } from "./functions.js";
+import { filtrarTarefasComcluidas } from "./functions.js";
+import { comcluirTarefa } from "./functions.js";
